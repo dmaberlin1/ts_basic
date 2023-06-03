2399783dfa5ffce50096f9b5ecb83f89698d940d
dmaberlin1 <dmaberlin77@gmail.com>
2023-06-03 12:12:32 +0300

    init: basic generic's

--- app.ts
+++ app.ts
@@ -1,6 +1,266 @@
-/*****************************************ПРОДВИНУТЫЕ ТИПЫ**************************************
+// **********************************-07- Generic - Дженерики*******************************
+// **********************************-07- Generic - Дженерики*******************************
+// **********************************-07- Generic - Дженерики*****************************/
+
+//****************************Mixins***********************************
+//****************************Mixins***********************************
+//****************************Mixins***********************************
+// Миксины реалиуются как наследование от нескольких классов
+// Либо добавление примесы дополнительных свойств, тому или иному обьекту без явного наследования
+//используются активно в DCI тестировании
+//
+// type Constructor= new(...args:any[])=>{
+//
+// }
+// type GConstructor<T={}>=new(...args:any[])=>{
+// }
+// class List{
+//     constructor(public items:string[]) {
+//     }
+// }
+//
+// type ListType=GConstructor<List>;
+//
+// class ExtendedListClass extends List{
+//     first(){
+//         return this.items[0];
+//     }
+// }
+//
+// function E() {
+//
+// }
+
+
+//*****************************************************generic классы**************************
+//*****************************************************generic классы**************************
+// дженерики в классе полезны когда есть некоторые универсальные верхние части обьекта, такие как status code и тп
+// а внутри есть переменные поля, которые зависят от реализации
+
+//
+// class Resp<D,E> {
+//     data?:D;
+//     error?:E;
+//
+//     constructor(data?:D,error?:E) {
+//         if(data){
+//             this.data=data
+//         }
+//         if(error){
+//             this.error=error;
+//         }
+//     }
+// }
+//
+//
+// const res=new Resp<string,number>('data',0)
+//
+// class HttpResp extends Resp<string,number>{
+//     code:number;
+//     setCode(code:number){
+//         this.code=code;
+//     }
+// }
+//
+// const res2=new HttpResp();
+// res2.code
+
+//*****************************************************ограничение generic**************************
+//*****************************************************ограничение generic**************************
+// мы не можем работать с genericом как с определенным типом, и обращаться например к его свойствам
+
+// class Vehicle{
+//     run:number;
+// }
+// interface Vehicle{
+//     run:number;
+// }
+//
+// function kmToMiles<T extends Vehicle>(vehicle:T) {
+//     vehicle.run=vehicle.run/0.62;
+//     return vehicle
+// }
+//
+// class LCV extends Vehicle{
+//     capacity:number;
+// }
+// interface LCV extends Vehicle{
+//     capacity:number;
+// }
+// const vehicle=kmToMiles(new Vehicle());
+// const lcv=kmToMiles(new LCV());
+//
+//
+// function logId<T extends string|number>(id:T):T {
+//     console.log(id)
+//     return id;
+// }function logId1<T extends string|number,Y>(id:T,additionalData:Y):{id:T,data:Y} {
+//     console.log(id)
+//     return {id,data:additionalData};
+// }
+
+
+
+//***********************функция сортировки любых обьектов, которые имеют id по убыванию и возрастанию
+
+
+// const data=[
+// {id:1,name:'dma1'},
+// {id:2,name:'dma2'},
+// {id:3,name:'dma3'},
+// ]
+//
+// interface ID{
+//     id:number;
+// }
+//
+// function sort<T extends ID>(data:T[],type:'asc'|'desc'='asc'):T[] {
+//     return data.sort((a,b)=>{
+//         switch (type) {
+//             case "asc":
+//                 return a.id - b.id
+//
+//             case "desc":
+//                 return b.id-a.id
+//         }
+//     })
+// }
+//
+// console.log(sort(data))
+// console.log(sort(data,'desc'))
+
+// ***************************************Использование в типах***************************************
+// ***************************************Использование в типах**************************
+//
+// interface ILogLine<T> {
+//     timeStamp:Date;
+//     data:T
+// }
+//
+// type LogLineType<T>={
+//     timeStamp:Date;
+//     data:T
+// }
+//
+// const logLine:LogLineType<{ a: number }>={
+//     timeStamp: new Date(),
+//     data:{
+//         a:1
+//     }
+// }
+
+
+
+//***функция преобразования в строку
+
+// function toString<T>(data:T):string|undefined {
+//     if(Array.isArray(data)){
+//         return data.toString();
+//     }
+//     switch (typeof data) {
+//         case "string":
+//             return data;
+//         case 'number':
+//         case "symbol":
+//         case "bigint":
+//         case "boolean":
+//         case "function":
+//             return data.toString();
+//         case "object":
+//             return JSON.stringify(data);
+//         default:
+//             return undefined;
+//
+//     }
+// }
+//
+// console.log(toString(3))
+// console.log(toString({'1':'2','3':'4'}))
+// console.log(toString(true))
+//
+//
+
+//*************************Встроенные generики***********
+//благодяря дженерику мы можем делать универсальные фции с точки зрения типов
+
+// const num:Array<number>=[1,2,3]
+//
+// async function test() {
+//     const a=await new Promise<number>((resolve, reject)=>{
+//         resolve(1)
+//     })
+// }
+//
+// //правильная типизация , не ограниченного числом строк и значений обьекта
+// const check:Record<string,boolean>={
+//     drive:true,
+//     kpp:false
+// }
+//
+//
+// function logMiddleware<T>(data:T):T {
+//     console.log(data)
+//     return data;
+// }
+//
+// const res=logMiddleware<number>(10);
+//
+//
+// function getSplitedHalf<T>(data:Array<T>):Array<T> {
+//     const length=data.length/2;
+//     return data.splice(0,length)
+// }
+//
+// getSplitedHalf<number|string|boolean>([1,'2','3',true,33n])
+//
+//
+
+/*****************************************-04-ПРОДВИНУТЫЕ ТИПЫ**************************************
  /*****************************************ПРОДВИНУТЫЕ ТИПЫ**************************************
- /*****************************************ПРОДВИНУТЫЕ ТИПЫ**************************************
+ /*****************************************ПРОДВИНУТЫЕ ТИПЫ**************************************/
+
+
+ // **********************Type Guard******************************************************************
+    // *******************Type Guard***************************************************************/
+//
+// function logId(id:string|number) {
+//     if(isString(id)) console.log(id)
+//     else console.log(id+' - not string')
+// }
+//
+// function isString(x:string|number):x is string {
+//     return typeof x==='string'
+// }
+//
+// interface User{
+//     name:string,
+//     login:string,
+//     id:number
+// }
+// interface Admin{
+//     name:string;
+//     role:number;
+//     position:string
+// }
+// //роль есть только у админа, и таким методом можно проверить что это точно админ
+// function isAdmin(user: User|Admin):user is Admin {
+//     return 'role' in user
+// }
+//
+// function isAdminAlternative(user:User|Admin):user is Admin {
+//     return (user as Admin).role !==undefined
+// }
+// //Этот метод юзаем когда не можем добраться до role in user, в некоторых случаях
+//
+// function setPosition(user:User|Admin) {
+//     if(isAdmin(user)) user.position='developer'
+//     // else user.id=1;
+//     else throw new Error('User is not Admin')
+// }
+//
+//
+
+
 
  //********************************************************UNION*************************************
  //********************************************************UNION*************************************/
@@ -88,7 +348,6 @@
 
 //******************************Interfaces**********************************************************
 //******************************Interfaces**********************************************************
-//******************************Interfaces**********************************************************
 //кроме прочего интерфейсы нужно екстендить,  тайпы через амперсанд
 // interface User {
 //     name:string,
@@ -125,7 +384,6 @@
 
 //********************************Типы и Интерфейсы - разница*********************************
 //********************************Типы и Интерфейсы - разница*********************************
-//********************************Типы и Интерфейсы - разница*********************************
 
 
 // interface User {
@@ -157,7 +415,6 @@
 
 
 
-// ***********************************Optional*****************************
 // ***********************************Optional*****************************
 // ***********************************Optional*****************************/
 
@@ -260,18 +517,56 @@
 // ***********************************Never****************************
 // ***********************************Never*****************************
 // позволяет описать кейсы где мы никогда не вернёмся
-    function genereateError(message:string):never {
-    throw new Error(message)
-    }
+// использовать его для проверки кейсов, пытаться присвоить переменной типа never , кейс в дефолте
+// Так как основная цель Тс, исключить ошибки в рантайме, и решить их в компайлед тайм
+// так же кейс- исчерпывающая проверка с кейс фции isString- описанной ниже
+// если в js рантайме будет что то другое, можно ссылаться на фцию ошибку
+// если есть какая то цепочка ифоф, и что то пойти не так, можно сделать прооверку в фцией возвращаюей never
+//     function generateError(message:string):never {
+//     throw new Error(message)
+//     }
+//
+// function generateError1(message:string):never {
+//     throw new Error(message)
+// }
+//     function isString(x:string|number):boolean {
+//         if(typeof x==='string') return true
+//         else if( typeof x ==='number') return false
+//         generateError1('error')
+//     }
+//
+//
+//     function dumpError():never {
+//         while (true){}
+//     }
+//
+//     type paymentAction='refund'|'checkout'|'reject';
+//     function processAction(action:paymentAction) {
+//         switch (action) {
+//             case 'refund':
+//                 //...
+//             break;
+//             case 'checkout':
+//                 //...
+//                 break;
+//             default:
+//                 //если кейсы не доходят до дефолта, то и ошибка с never подсвечиваться не будет
+//                 const _:never=action;
+//                 throw new Error('Нет такого action')
+//         }
+//     }
+//
+//
 
-    function dumpError():never {
-        while (true){}
-    }
 
-
-
-
-
+// ***********************************Null****************************
+// ***********************************Null*****************************
+    // null -явно заданный неопределенный обьект, undefined же можно полчить в рамках выполнения нашего кода
+    // делаем явный возврат  при неподходящем условии в фции
+    // тогда null укажет нам на явное отстутсвтие, undefined же может указать на не ясные данные
+    // делаем проверку,  есть или нету, если нету в случае null- будет ошибка, в случае undefined не будет
+    //юзаем всегда use strict null check , если нет осознанного обьекта, возрващаем null, null означате что
+    // этого обьекта осознанно нет, undefined же укажет что нет свойства и тп, и можно получить его в рантайме
 
 
 // *********************************************************основы**********************************
@@ -311,7 +606,6 @@
 
 //*********************************************************Tuples - кортежи**********************************
 //*********************************************************Tuples - кортежи**********************************
-//*********************************************************Tuples - кортежи**********************************
 //с точки зрения js- это просто массив
 // Кортежи удобны тем что можно ограничить его элементы, но  в него можно пушить , делать pop и тп
 // const skill:[number,string]=[1,'Dev'];
@@ -323,7 +617,6 @@
 // const array:readonly [number,string,...boolean[]]=[1,'test',true,false];
 
 
-//*********************************************************ENUMS**********************************
 //*********************************************************ENUMS**********************************
 //*********************************************************ENUMS**********************************
 //енамы во многом себя ведут похоже на обьекты
